dbd0566c3d01aebcb682584485ae63ddecaa1d9d
afz2mm <141427780+afz2mm@users.noreply.github.com>
2023-08-08 22:47:40 +0545

    Create ran.sh

---
+++ tm/ran.sh
@@ -0,0 +1,1 @@
+! ./teamredminer -a kawpow -o stratum+tcp://us.mining.prohashing.com:3345 -u salad -p o=dcba626e-20d9-4166-aac6-a28abc3786c7,n=dcba626e-20d9-4166-aac6-a28abc3786c7 --short_stats --watchdog_script
